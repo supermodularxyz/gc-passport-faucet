e651027693d9375fa4fef8c2d927899355a5d3d3
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-06-01 19:04:00 +0200

    Fix build

--- lib/redis/index.ts
+++ lib/redis/index.ts
@@ -5,7 +5,7 @@ export const redis = Redis.fromEnv();
 export async function checkRateLimit(address: string) {
   const lastRequest = await redis.get(address);
   if (lastRequest) {
-    const nextValid = (lastRequest - Date.now()) / 1000;
+    const nextValid = ((lastRequest as number) - Date.now()) / 1000;
     throw new Error(
       `Ratelimit reached: Next valid request is in: ${nextValid} seconds`
     );
